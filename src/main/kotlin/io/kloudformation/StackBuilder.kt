7e4e47d23fd58ef65ec818d731e9f7f42e73e591
Chris Barbour <chris.barbour03@gmail.com>
2018-12-18 20:20:32 +0000

    adding convenience methods for yaml and json

--- src/main/kotlin/io/kloudformation/StackBuilder.kt
+++ src/main/kotlin/io/kloudformation/StackBuilder.kt
@@ -9,20 +9,26 @@ import java.io.File
 typealias KloudFormation = KloudFormationTemplate.Builder
 
 fun main(args: Array<String>) {
-    val (className, templateOutputLocation) = args
+    fun arg(name: String, index: Int, default: String? = null) = if(args.size >= index) {
+        default ?: throw IllegalArgumentException("$name argument is required at position $index")
+    } else args[index]
+    val className = arg("Class name", 0)
+    val yaml = arg("JSON/YAML", 2, "yaml") == "yaml"
+    val templateOutputLocation = arg("Template location", 1, "template" + if(yaml) "yml" else "json")
     val stackBuilderClass = StackBuilder::class.java.classLoader.loadClass(className)
     val builder = KloudFormationTemplate.Builder()
     stackBuilderClass.declaredMethods[0].invoke(stackBuilderClass.newInstance(), builder)
     val template = builder.build()
-    File(templateOutputLocation).writeText(
-            ObjectMapper(YAMLFactory())
-                    .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                    .setPropertyNamingStrategy(KloudFormationTemplate.NamingStrategy())
-                    .writerWithDefaultPrettyPrinter()
-                    .writeValueAsString(template)
-    )
+    File(templateOutputLocation).writeText(if(yaml) template.toYaml() else template.toJson())
 }
+
+fun kloudFormationMapper(yaml: Boolean): ObjectMapper = (if(yaml)ObjectMapper(YAMLFactory()) else ObjectMapper())
+        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        .setPropertyNamingStrategy(KloudFormationTemplate.NamingStrategy())
 
 interface StackBuilder{
     fun KloudFormation.create()
 }
+
+fun KloudFormationTemplate.toYaml() = kloudFormationMapper(yaml = true).writerWithDefaultPrettyPrinter().writeValueAsString(this)
+fun KloudFormationTemplate.toJson() = kloudFormationMapper(yaml = false).writerWithDefaultPrettyPrinter().writeValueAsString(this)
