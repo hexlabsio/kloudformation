b289b46f23c2385b0411958bca3022cafc1cc706
Chris Barbour <chris.barbour03@gmail.com>
2019-12-18 07:21:06 +0000

    adding all regions back in

--- src/main/kotlin/io/kloudformation/specification/SpecificationScraper.kt
+++ src/main/kotlin/io/kloudformation/specification/SpecificationScraper.kt
@@ -4,11 +4,10 @@ import org.jsoup.Jsoup
 
 object SpecificationScraper {
     fun scrapeLinks(specificationListUrl: String): Map<String, String> =
-        listOf(Jsoup.connect(specificationListUrl)
+            Jsoup.connect(specificationListUrl)
             .get()
             .select(".table-contents tr td p").toList()
             .chunked(4)
             .map { it[0].text() to it[2].child(0).attr("href") }
-                .first())
             .toMap()
 }
