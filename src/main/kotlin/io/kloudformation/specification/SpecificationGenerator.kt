19b703f6f57f1f4b24ddc835c6d96d02d007b1cd
Chris Barbour <chris.barbour03@gmail.com>
2020-07-12 10:50:30 +0100

    updating for changes in aws

--- src/main/kotlin/io/kloudformation/specification/SpecificationGenerator.kt
+++ src/main/kotlin/io/kloudformation/specification/SpecificationGenerator.kt
@@ -1,15 +1,18 @@
 package io.kloudformation.specification
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.kloudformation.specification.SpecificationMerger.merge
 import java.net.URL
 import java.util.zip.GZIPInputStream
 
-private val jacksonObjectMapper = com.fasterxml.jackson.module.kotlin.jacksonObjectMapper().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-private val specificationListUrl = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-resource-specification.partial.html"
+private val jacksonObjectMapper = com.fasterxml.jackson.module.kotlin.jacksonObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+private val specificationListUrl = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-resource-specification.html"
 
-fun main(args: Array<String>) {
+fun main() {
     SpecificationPoet.generate(SpecificationScraper
         .scrapeLinks(specificationListUrl)
         .mapNotNull { (specificationName, specificationUrl) ->
